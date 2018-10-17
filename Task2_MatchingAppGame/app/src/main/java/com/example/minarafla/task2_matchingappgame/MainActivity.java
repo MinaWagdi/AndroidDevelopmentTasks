dd15ccf273097cc7ef1093c7a9ec8c77a84a2890
MinaWagdi <MinaWagdi@users.noreply.github.com>
2018-10-17 19:25:04 +0200

    Animals Sounds added, rotation locked, database for scores finished.

--- Task2_MatchingAppGame/app/src/main/java/com/example/minarafla/task2_matchingappgame/MainActivity.java
+++ Task2_MatchingAppGame/app/src/main/java/com/example/minarafla/task2_matchingappgame/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.minarafla.task2_matchingappgame;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,11 +9,14 @@ import android.view.View;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+//    static DBAdapter myDb;
 
     ImageView im1;
     ImageView im2;
@@ -21,7 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ImageView im6;
     ImageView im7;
     ImageView im8;
-    int imageNum;
+
 
     int[] AnimalsImgsArray;
     List<Integer> AnimalsPics;
@@ -35,9 +40,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     String picture1Resource;
     String picture2Resource;
 
-    int score;
 
+    MediaPlayer ring;
+    boolean ringing;
+    //for the logcat only
     int counter=0;
+
+    final int NumberOfCards = 8;
+    int numberOfMatchedCards=0;
+    String score="THE BEST MINA WAGDI";
+    String date ="today";
+
+    Date date1;
+    Date date2;
 
 
     @Override
@@ -45,9 +60,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageNum = 8;
+
+//        openDB();
+
+        date1 = Calendar.getInstance().getTime();
+
         ReferenceImageViews();
-        InitializeImageViews();
+        setResourcesForImageViews();
+
+        //getArrayImages() is the same as InitializeAnimalsPicsArrayList()
+        //The only difference is that the images in the first one are stored in an array
+        //While in the second, the images are stored in an ArrayList so we can shuffle them.
         AnimalsImgsArray = getArrayImages();
         AnimalsPics = new ArrayList<Integer>();
         InitializeAnimalsPicsArrayList();
@@ -75,7 +98,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         im8 = findViewById(R.id.image8);
     }
 
-    public void InitializeImageViews(){
+    public void setResourcesForImageViews(){
         im1.setImageResource(R.drawable.card_back);
         im2.setImageResource(R.drawable.card_back);
         im3.setImageResource(R.drawable.card_back);
@@ -101,43 +124,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        int i;
+        if(ringing){
+            //ring.reset();
+            ring.release();
+            ringing=false;
+        }
+        int imageIndex;
+        //check if the same image is clicked twice, so we flip it.
         if(checkIfSameImageClicked(v)){
             return;
         }
+        //If only one card is flipped--> then flip another one
         else if((firstClickFlag!=secondClickFlag)||(firstClickFlag==false && secondClickFlag==false)){
-            i=ShowClickedImage(v);
+            imageIndex=ShowClickedImage(v);
             if(firstClickFlag==true&&secondClickFlag==false) {
                 secondImageClickedID=v.getId();
                 secondClickFlag = true;
-                picture2Resource=AnimalsPics.get(i).toString();
-                if(imagedMatched()){
+                picture2Resource=AnimalsPics.get(imageIndex).toString();
+                if(imagesMatched()){
+                    //this functiion will hide the 2 similar images.
+                    //note in its implementation they are all if conditions, not if else conditions.
                     HideMatchedImages(picture1Resource);
-                    //HideMatchedImages(picture2Resource);
+                    if(numberOfMatchedCards==8){
+                        date2= Calendar.getInstance().getTime();
+                        long d = date2.getTime()-date1.getTime();
+                        score=""+(d*0.001)+" seconds";
+                        date=date1.toString();
+                        ring.release();
+                        long res=MainMenu.insertData(date,""+score);
+                        if(res==0){
+                            Log.i("DBmm","Data not inserted successfully");
+                        }
+                        else{
+                            Log.i("DBmm","Data inserted successfully");
+                        }
+                        Intent intent = new Intent(getApplicationContext(),ShowGameScore.class);
+                        intent.putExtra("score",score);
+                        startActivity(intent);
+                    }
                     Log.i("WINNER","GREAT");
                 }
-                Log.i("MINASTAG",counter+" first click flag is "+firstClickFlag+" and second click flag is "+secondClickFlag);
-                counter++;
             }
             //In the beginning only that the both flags should be equal false
             else if(firstClickFlag==false && secondClickFlag==false){
                 firstImageClickedID=v.getId();
                 firstClickFlag=true;
-                picture1Resource=AnimalsPics.get(i).toString();
-                Log.i("MINASTAG",counter+" first click flag is "+firstClickFlag+" and second click flag is "+secondClickFlag);
-                counter++;
+                picture1Resource=AnimalsPics.get(imageIndex).toString();
             }
         }
+        //if two cards are flipped
         else if(firstClickFlag==true && secondClickFlag==true){
+            imageIndex=ShowClickedImage(v);
             HideImage(firstImageClickedID);
             HideImage(secondImageClickedID);
-            i=ShowClickedImage(v);
             secondClickFlag = false;
             firstClickFlag=true;
             firstImageClickedID=v.getId();
-            picture1Resource=AnimalsPics.get(i).toString();
-            Log.i("MINASTAG",counter+" first click flag is "+firstClickFlag+" and second click flag is "+secondClickFlag);
-            counter++;
+            picture1Resource=AnimalsPics.get(imageIndex).toString();
         }
     }
 
@@ -175,75 +218,94 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public int ShowClickedImage(View v){
         int image_num=0;
+        String imageName;
         switch (v.getId()) {
             case R.id.image1:
                 im1.setImageResource(AnimalsPics.get(0));
                 image_num=0;
-                Log.i("MINASTAG",""+AnimalsPics.get(0).toString());
+                runAnimalSound(getResources().getResourceName(AnimalsPics.get(image_num)));
+                Log.i("IMAGENAME",""+getResources().getResourceName(AnimalsPics.get(image_num)));
                 break;
             case R.id.image2:
                 im2.setImageResource(AnimalsPics.get(1));
                 image_num=1;
-                Log.i("MINASTAG",""+AnimalsPics.get(1).toString());
+                runAnimalSound(getResources().getResourceName(AnimalsPics.get(image_num)));
+                Log.i("IMAGENAME","Hello "+getResources().getResourceName(AnimalsPics.get(image_num)));
                 break;
             case R.id.image3:
                 im3.setImageResource(AnimalsPics.get(2));
                 image_num=2;
-                Log.i("MINASTAG",""+AnimalsPics.get(2).toString());
+                runAnimalSound(getResources().getResourceName(AnimalsPics.get(image_num)));
+                Log.i("IMAGENAME",""+getResources().getResourceName(AnimalsPics.get(image_num)));
                 break;
             case R.id.image4:
                 im4.setImageResource(AnimalsPics.get(3));
                 image_num=3;
-                Log.i("MINASTAG",""+AnimalsPics.get(3).toString());
+                runAnimalSound(getResources().getResourceName(AnimalsPics.get(image_num)));
+                Log.i("IMAGENAME",""+getResources().getResourceName(AnimalsPics.get(image_num)));
                 break;
             case R.id.image5:
                 im5.setImageResource(AnimalsPics.get(4));
                 image_num=4;
-                Log.i("MINASTAG",""+AnimalsPics.get(4).toString());
+                runAnimalSound(getResources().getResourceName(AnimalsPics.get(image_num)));
+                Log.i("IMAGENAME",""+getResources().getResourceName(AnimalsPics.get(image_num)));
                 break;
             case R.id.image6:
                 im6.setImageResource(AnimalsPics.get(5));
                 image_num=5;
-                Log.i("MINASTAG",""+AnimalsPics.get(5).toString());
+                runAnimalSound(getResources().getResourceName(AnimalsPics.get(image_num)));
+                Log.i("IMAGENAME",""+getResources().getResourceName(AnimalsPics.get(image_num)));
                 break;
             case R.id.image7:
                 im7.setImageResource(AnimalsPics.get(6));
                 image_num=6;
-                Log.i("MINASTAG",""+AnimalsPics.get(6).toString());
+                runAnimalSound(getResources().getResourceName(AnimalsPics.get(image_num)));
+                Log.i("IMAGENAME",""+getResources().getResourceName(AnimalsPics.get(image_num)));
                 break;
             case R.id.image8:
                 im8.setImageResource(AnimalsPics.get(7));
                 image_num=7;
-                Log.i("MINASTAG",""+AnimalsPics.get(7).toString());
+                runAnimalSound(getResources().getResourceName(AnimalsPics.get(image_num)));
+                Log.i("IMAGENAME",""+getResources().getResourceName(AnimalsPics.get(image_num)));
                 break;
         }
         return image_num;
     }
 
     public void HideMatchedImages(String res){
+        ring.release();
+        ringing=false;
         if(AnimalsPics.get(0).toString().equalsIgnoreCase(res)){
             im1.setVisibility(View.INVISIBLE);
+            numberOfMatchedCards++;
         }
         if(AnimalsPics.get(1).toString().equalsIgnoreCase(res)){
             im2.setVisibility(View.INVISIBLE);
+            numberOfMatchedCards++;
         }
         if(AnimalsPics.get(2).toString().equalsIgnoreCase(res)){
             im3.setVisibility(View.INVISIBLE);
+            numberOfMatchedCards++;
         }
         if(AnimalsPics.get(3).toString().equalsIgnoreCase(res)){
             im4.setVisibility(View.INVISIBLE);
+            numberOfMatchedCards++;
         }
         if(AnimalsPics.get(4).toString().equalsIgnoreCase(res)){
             im5.setVisibility(View.INVISIBLE);
+            numberOfMatchedCards++;
         }
         if(AnimalsPics.get(5).toString().equalsIgnoreCase(res)){
             im6.setVisibility(View.INVISIBLE);
+            numberOfMatchedCards++;
         }
         if(AnimalsPics.get(6).toString().equalsIgnoreCase(res)){
             im7.setVisibility(View.INVISIBLE);
+            numberOfMatchedCards++;
         }
         if(AnimalsPics.get(7).toString().equalsIgnoreCase(res)){
             im8.setVisibility(View.INVISIBLE);
+            numberOfMatchedCards++;
         }
 
 
@@ -279,12 +341,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
 
-    public boolean imagedMatched(){
+    public boolean imagesMatched(){
         Log.i("WINNER",""+picture1Resource.toLowerCase().toString());
         Log.i("WINNER",""+picture2Resource.toLowerCase().toString());
         if(picture1Resource.toString().equalsIgnoreCase(picture2Resource.toString()))
             return true;
         return false;
+    }
+
+    public void runAnimalSound(String imageName){
+        ringing=true;
+        if(imageName.equalsIgnoreCase("com.example.minarafla.task2_matchingappgame:drawable/chicken1")){
+            ring= MediaPlayer.create(MainActivity.this,R.raw.chicken_noise);
+            ring.start();
+            Log.i("ANIMALSOUND","ENTERED in chicken");
+
+        }
+        else if(imageName.equalsIgnoreCase("com.example.minarafla.task2_matchingappgame:drawable/dog1")){
+            ring= MediaPlayer.create(MainActivity.this,R.raw.dog_sound);
+            ring.start();
+            Log.i("ANIMALSOUND","ENTERED in dog");
+
+        }
+        else if(imageName.equalsIgnoreCase("com.example.minarafla.task2_matchingappgame:drawable/monkey1")){
+            ring= MediaPlayer.create(MainActivity.this,R.raw.monkey_sound);
+            ring.start();
+            Log.i("ANIMALSOUND","ENTERED in monkey");
+
+        }
+        else if(imageName.equalsIgnoreCase("com.example.minarafla.task2_matchingappgame:drawable/zibra1")){
+            ring= MediaPlayer.create(MainActivity.this,R.raw.donkey_sound);
+            ring.start();
+            Log.i("ANIMALSOUND","ENTERED in donkey");
+
+        }
     }
 
 
@@ -296,6 +386,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     *
     *
     * */
+
+//
+//    private void openDB() {
+//        myDb = new DBAdapter(this);
+//        myDb.open();
+//    }
+//    private void closeDB() {
+//        myDb.close();
+//    }
+//
+//    public long insertData(String date, String score) {
+//
+//        long newId = myDb.insertRow(date,score);
+//        Log.i("DB","newId "+newId);
+//        if(newId==-1){
+//            return  0;
+//        }
+//        else{
+//            return 1;
+//        }
+//    }
 }
 
 
